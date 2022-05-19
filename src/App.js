ef618c3d674f3767eaa714b0c89b5b5d5d4d6dc5
hjoyheath <hillaryjoyheath@gmail.com>
2022-05-19 17:21:53 -0400

    book homepage detail

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import initialBookState from './bookData'; 
+import './App.css';
+
+function Book({ title, author, shortDescription, coverImageUrl}) {
+  return (
+      <div className="book">
+        <h1 className="book-name">{title}</h1>
+        <h2 className="book-author">{author}</h2>
+        <p className="book-description">{shortDescription}</p>
+        <img src={coverImageUrl} alt={title} className="book-image"></img>
+
+      </div>
+
+  )
+}
+
+function App() {
+  const [books, setBooks] = useState(initialBookState);
+  return (
+    <div className="container">
+         <h1>Books to Read</h1> 
+      {books.map((book) => (
+        <Book {...book} key={book.id} />
+      ))}
+    </div>
+  );
+}
+
+
+export default App;
